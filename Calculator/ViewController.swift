8894ea10d74586a2d9f48b75d9826a288e97c9f5
Jason Wang <vuwij@me.com>
2016-08-20 20:00:27 -0400

    First Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func keyPressed(sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("Touched \(digit) digit")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
